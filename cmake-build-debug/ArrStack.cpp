955b4ac5dc190dda623f1fd29a77d6130b0f5ce9
Andrew Adel <android8giga@gmail.com>
2022-04-18 14:00:32 +0200

    Initialize Array Stack

---
+++ cmake-build-debug/ArrStack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andro on 18/4/2022.
+//
+
+#include "ArrStack.h"
---
+++ cmake-build-debug/ArrStack.h
@@ -0,0 +1,14 @@
+//
+// Created by andro on 18/4/2022.
+//
+
+#ifndef DATASTRUCTURES_ARRSTACK_H
+#define DATASTRUCTURES_ARRSTACK_H
+
+
+class ArrStack {
+
+};
+
+
+#endif //DATASTRUCTURES_ARRSTACK_H
